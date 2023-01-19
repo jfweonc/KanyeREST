ecc047f48f106a376efcccb5e92772df87bed7a7
Guolong Li <Guolong.LiRealtor@gmail.com>
2023-01-18 19:57:04 -0600

    KanyeREST

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Newtonsoft.Json.Linq;
+using System.Runtime.Intrinsics.X86;
+
+Console.WriteLine("Hello, World!");
+var kanyeURL = "https://api.kanye.rest";
+var client1 = new HttpClient();
+var kanyeResponse = client1.GetStringAsync(kanyeURL).Result;
+var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+Console.WriteLine(kanyeQuote); 
+
+var ronURL = "https://api.kanye.rest";
+var client2 = new HttpClient();
+var ronResponse = client2.GetStringAsync(ronURL).Result;
+var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+Console.WriteLine(ronQuote);
